97f46b1f6a15d8e7f4fe0d9f0a2b844507786672
Eliane Discacciati <discacciati@gmail.com>
2022-09-26 19:37:57 -0300

    feat: documentation

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,15 @@ import "dotenv/config";
 import userRoute from "./routes/user.routes";
 import contactRoute from "./routes/contact.routes";
 import handleAppErrorMiddeware from "./middlewares/handleAppErrorMiddleware";
+import swaggerUI from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 
 const app = express();
 app.use(express.json());
 
 app.use("/user", userRoute);
 app.use("/contact", contactRoute);
+app.use("api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use(handleAppErrorMiddeware);
 
